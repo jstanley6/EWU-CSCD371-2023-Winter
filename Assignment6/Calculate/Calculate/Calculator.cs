82a1c7df244e40fb6b1a075cf9e47ec0a7d80400
Jason Stanley <jstanley3@ewu.edu>
2023-02-20 18:57:32 -0800

    Set up file structure
    Set up file structure for branch and created Calculator class.

---
+++ Assignment6/Calculate/Calculate/Calculator.cs
@@ -0,0 +1,51 @@
+﻿namespace Calculate;
+
+public class Calculator
+{
+    private delegate int OperationDelegate(int a, int b);
+    
+        private static readonly char[] Operators = new[] {'+', '-', '*', '/'};
+    
+        private static readonly IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations = new Dictionary<char, Func<int, int, int>>
+        {
+            {Operators[0], Add},
+            {Operators[1], Subtract},
+            {Operators[2], Multiply},
+            {Operators[3], Divide}
+        };
+    
+        private static int Add(int a, int b)
+            => a + b;
+    
+        private static int Subtract(int a, int b)
+            => a - b;
+    
+        private static int Multiply(int a, int b)
+            => a * b;
+    
+        private static int Divide(int a, int b)
+            => a / b;
+    
+        public static bool TryCalculate(string calculation, out int result)
+        {
+            var split = calculation.Split(" ");
+            int a;
+            int b;
+    
+            if (split.Length != 3 ||
+                !int.TryParse(split[0], out a) || !int.TryParse(split[2], out b) ||
+                split[1].Length != 1 || Operators.All(o => o != split[1].ToCharArray()[0]))
+            {
+                result = 0;
+                return false;
+            }
+    
+            var symbol = split[1].ToCharArray()[0];
+            result = MathematicalOperations[symbol](a, b);
+
+            return true;
+        }
+    
+
+
+}
