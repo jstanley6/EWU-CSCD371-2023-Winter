85a35b479a90f99a0beece1a0e23f66a5763b1df
Jason Stanley <jstanley3@ewu.edu>
2023-02-21 00:02:45 -0800

    Project messed up when doing the directory.props file

--- Assignment6/Calculate/Calculate/Program.cs
+++ Assignment6/Calculate/Calculate/Program.cs
@@ -1,3 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace Calculate;
+
+using System;
+
+public class Program
+{
+    public Program()
+    {
+        Writeline = Console.WriteLine;
+        Readline = Console.ReadLine;
+    }
+    
+    public Action<string> Writeline { get; init; }
+    public Func<string?> Readline { get; init; }
+
+    public static void Main(string[] args)
+    {
+        var calculator = new Calculator();
+        bool exit = false;
+        Program program = new();
+
+        do
+        {
+            program.Writeline("Enter mathematical equation in the form of x + y." +
+                              "\nType quit to exit: ");
+
+            var input = program.Readline();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                break;
+            }
+            if (input is "quit")
+            {
+                exit = true;
+                program.Writeline("Quitting program..");
+            }
+            else if (calculator.TryCalculate(input, out var result))
+            {
+                program.Writeline($"The input is {input} and the result is {result}");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Please try again.");
+            }
+        } while (!exit);
+    }
+}
